1ec3587a300077e506f60f74869fae82cb3cd4ec
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-27 00:58:50 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,16 @@
   <div id="app">
     <div class="home-head">
       <div class="home-head-bgimg">
-        <img src="http://pa74otoy6.bkt.clouddn.com/pd-title-background.png" alt="">
-        <img src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="">
+        <img class="home-head-bgimg1" src="http://pa74otoy6.bkt.clouddn.com/pd-title-background.png" alt="">
+        <img class="home-head-bgimg2" src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="">
       </div>
-      <el-menu class="el-menu-demo" active-text-color="#81b316" background-color="#ffffff" default-active="首页" :router="true" mode="horizontal">
-        <el-menu-item :index="data.title" v-for="(data,key) in menu" :key="key" :label="key" :route="data.path">
-          {{data.title}}
-        </el-menu-item>        
-      </el-menu>
+      <div class="home-head-menu">
+        <el-menu class="el-menu-demo" active-text-color="#81b316" background-color="#ffffff" default-active="首页" :router="true" mode="horizontal">
+          <el-menu-item :index="data.title" v-for="(data,key) in menu" :key="key" :label="key" :route="data.path">
+            {{data.title}}
+          </el-menu-item>        
+        </el-menu>
+      </div>
     </div>
   <router-view></router-view>
     <div class="home-foot">
@@ -38,3 +40,29 @@ export default {
   }
 }
 </script>
+
+<style>
+html,
+body,
+#app{
+  margin: 0;
+  padding: 0;
+}
+.home-head-bgimg{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.home-head-bgimg1{
+  width: 100%;
+  position: absolute;
+  z-index: -1;
+}
+.home-head-bgimg2{
+  height: 50px;
+  width: 50px;
+}
+.home-head-menu{
+  
+}
+</style>
