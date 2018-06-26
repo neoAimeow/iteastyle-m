e30ec02d7f12e559c0d38d709ce32411f848ccb7
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-27 00:45:34 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,40 @@
 <template>
   <div id="app">
-    <div class="header">头部</div>
+    <div class="home-head">
+      <div class="home-head-bgimg">
+        <img src="http://pa74otoy6.bkt.clouddn.com/pd-title-background.png" alt="">
+        <img src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt="">
+      </div>
+      <el-menu class="el-menu-demo" active-text-color="#81b316" background-color="#ffffff" default-active="首页" :router="true" mode="horizontal">
+        <el-menu-item :index="data.title" v-for="(data,key) in menu" :key="key" :label="key" :route="data.path">
+          {{data.title}}
+        </el-menu-item>        
+      </el-menu>
+    </div>
   <router-view></router-view>
-    <div class="footer">尾部</div>
+    <div class="home-foot">
+      <div class="home-foot-left">
+        <div class="home-foot-left-logo"><img src="http://pa74otoy6.bkt.clouddn.com/opaque-logo.png" alt=""></div>
+        <div class="home-foot-left-order">
+          <span>全国订购电话</span>
+          <span>0571 8373 1743</span>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
+<script>
+import menus from './common/menus'
+export default {
+  name:'APP',
+  data(){
+    return{
+      menu: []
+    }
+  },
+  created(){
+    this.menu = menus
+  }
+}
+</script>
---
+++ src/common/menus.js
@@ -0,0 +1,33 @@
+const menus = [{
+    title: '首 页',
+    path: '/',
+    icon: ''
+},
+{
+    title: '公司故事',
+    path: '/homepage/companyStory',
+    icon: ''
+},
+{
+    title: '产品展示',
+    path: '/homepage/productDisplay',
+    icon: ''
+},
+{
+    title: '经典案例',
+    path: '/homepage/classicCase',
+    icon: ''
+},
+{
+    title: '茶式动态',
+    path: '/homepage/teaState',
+    icon: ''
+},
+{
+    title: '联系我们',
+    path: '/homepage/contactUs',
+    icon: ''
+}
+]
+
+export default menus
