2995f3586881de9dfbd1a559df144b6b7e103d65
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-26 22:27:16 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,10 +2,37 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import './registerServiceWorker'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import VueProgressiveImage from 'vue-progressive-image'
+import VueAMap from 'vue-amap'
 
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueProgressiveImage)
+Vue.use(VueAMap)
+
+let request = axios.create({
+  baseURL: 'https://iteastyle-api.aimeow.com/service',
+  timeout: 1000
+});
+
+Vue.prototype.$ajax = request
+
+/* eslint-disable no-new */
+VueAMap.initAMapApiLoader({
+  // 高德的key
+  key: '4566198fcdceda0404a449781377549e',
+  // 插件集合
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 高德 sdk 版本，默认为 1.4.4
+  v: '1.4.4'
+});
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  components: { APP },
+  el: '#app'
 }).$mount('#app')
