c8374956f6b6bb8541ea7e25684d8266afd50b12
sharon <403431641@qq.com>
2018-06-27 19:19:19 +0800

    pd

--- src/components/homepage/productDisplay.vue
+++ src/components/homepage/productDisplay.vue
@@ -1,5 +1,132 @@
 <template>
-    <div>
-        <h1>产品展示</h1>
+<div class="productDisplay">
+
+    <div class="pd-ct">           
+        <div class="product-name" v-for="(item,key) in items" :key="key">
+            <router-link :to="{path:'/homepage/productInside',query: {id: item.id}}">                        
+                <img class="pd-img" :src="item.imageArr[0]">                       
+                <span class="pd-span">{{item.title}}</span>
+            </router-link>
+        </div>
     </div>
+
+   <div class="pd-page">
+        <el-pagination
+            layout="prev, pager, next"
+            :total="totalCount" :page-size="pageSize" :current-page="currentPage" @current-change="currentPageChanged"
+        >
+        </el-pagination>
+   </div>
+
+</div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            items:[
+
+            ],
+            headerImageUrl:'',
+            currentPage:1,
+            totalCount:0,
+            pageSize:6
+        }
+ },
+created: function() {
+    this.request();
+  },
+  methods: {
+      currentPageChanged(val) {
+          this.currentPage = val;
+              this.request();
+        console.log(`当前页: ${val}`);
+      },
+      request() {
+              var that = this;
+            this.$ajax.get('/productShower', {
+                params: {
+                    page:this.currentPage,
+                    pageSize:this.pageSize
+                }
+
+            })
+            .then(function (response) {
+            console.log(response);
+            that.items = response.data.model.products;
+            that.totalCount = response.data.model.totalCount
+            that.headerImageUrl = response.data.model.headerImageUrl
+            })
+            .catch(function (response) {
+            console.log(response);
+            });
+      }
+  }
+}
+</script>
+
+
+<style scoped>
+a{
+    color:#626262;
+    text-decoration: none;
+}
+.productDisplay {
+    margin: 0;
+    padding: 0;
+    border-bottom:2px solid #9dc135;
+}
+
+.pd-title {
+    height: 150px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.pd-title-picture {
+    min-width: 1300px;
+    width: 100%;
+    height: 150px;
+    position: absolute;
+    z-index: -1;
+}
+
+.pd-title-logo {
+  text-align: center;
+}
+
+.pd-ct {
+  margin: 0 auto;
+  max-width: 600px;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center; 
+  margin-top: 40px;
+  margin-bottom: 20px;
+}
+
+
+
+.pd-img {
+  width: 265px;
+  height: 170px;
+  margin-bottom: 10px;
+}
+
+.product-name {
+  width:265px;
+  text-align: center;
+  margin: 15px;
+  color: #626262;
+}
+
+.pd-page {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 40px;
+}
+
+
+</style>
