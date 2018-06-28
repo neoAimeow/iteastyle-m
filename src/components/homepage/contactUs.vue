fc406ba1bd0e4dc61287972b35c0d10d5d64e70c
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-29 00:07:02 +0800

    update

--- src/components/homepage/contactUs.vue
+++ src/components/homepage/contactUs.vue
@@ -1,5 +1,50 @@
 <template>
-    <div>
-        <h1>联系我们</h1>
+    <div class="cu-container">
+        <div class="cu-title">
+            <span>OUR STORY</span>
+            <img src="" alt="">
+            <span>杭式下午茶</span>
+        </div>
+        <div class="cu-content">
+            <div class="cu-content-style">
+                <span>杭州执贝科技有限公司旗下品牌【杭式下午茶】是效力于茶歇（TEA BREAK）
+                    一站式的综合服务机构，真正以茶元素为核心，用纯天然的超微茶粉，将茶与饮品、茶与糕点
+                    、茶与时尚有机的结合在一起，旨在引领一种健康的生活方式与生活理念！
+                </span>
+            </div>
+        </div>
     </div>
 </template>
+
+<style>
+.cu-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.cu-title{
+    border-style: dotted;
+    border-color: #9dc135;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 150px;
+    height: 80px;
+    margin-top: 40px;
+}
+.cu-content{
+    width: 90%;
+    background-color: #eeebeb;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    margin-top: 40px;
+}
+.cu-content-style{
+    margin-top: 40px;
+    width: 80%;
+    margin-bottom: 40px
+}
+</style>
