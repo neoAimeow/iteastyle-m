728eea163cfa5c7168645754895b5e81f620ed67
Cross <cross@CrossdeMacBook-Pro.local>
2018-06-28 23:38:20 +0800

    update

--- src/components/homepage/teaState.vue
+++ src/components/homepage/teaState.vue
@@ -1,5 +1,79 @@
 <template>
-    <div>
-        <h1>茶式动态</h1>
+    <div class="ts-container">
+        <router-link class="ts-main" to="./teaStateInside">
+            <div class="ts-main-head">
+                <span>这份小确信，您想要么</span>
+            </div>
+            <div class="ts-main-center">
+                <span>转眼夏至，这一年已过去一半，正值夏天，闷热、阵雨、烈日似乎是个不太好的季节
+                    却也有人喜欢夏天的激情，阳光、海滩、冷饮，激情的夏日，一切活动仿如充满的干劲...
+                </span>
+            </div>
+        </router-link>
+        <router-link class="ts-main" to="./teaStateInside">
+            <div class="ts-main-head">
+                <span>这份小确信，您想要么</span>
+            </div>
+            <div class="ts-main-center">
+                <span>转眼夏至，这一年已过去一半，正值夏天，闷热、阵雨、烈日似乎是个不太好的季节
+                    却也有人喜欢夏天的激情，阳光、海滩、冷饮，激情的夏日，一切活动仿如充满的干劲...
+                </span>
+            </div>
+        </router-link>
+        <router-link class="ts-main" to="./teaStateInside">
+            <div class="ts-main-head">
+                <span>这份小确信，您想要么</span>
+            </div>
+            <div class="ts-main-center">
+                <span>转眼夏至，这一年已过去一半，正值夏天，闷热、阵雨、烈日似乎是个不太好的季节
+                    却也有人喜欢夏天的激情，阳光、海滩、冷饮，激情的夏日，一切活动仿如充满的干劲...
+                </span>
+            </div>
+        </router-link>
+        <router-link class="ts-main" to="./teaStateInside">
+            <div class="ts-main-head">
+                <span>这份小确信，您想要么</span>
+            </div>
+            <div class="ts-main-center">
+                <span>转眼夏至，这一年已过去一半，正值夏天，闷热、阵雨、烈日似乎是个不太好的季节
+                    却也有人喜欢夏天的激情，阳光、海滩、冷饮，激情的夏日，一切活动仿如充满的干劲...
+                </span>
+            </div>
+        </router-link>
     </div>
 </template>
+
+<style>
+a{
+    text-decoration: none;
+}
+.ts-container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.ts-main{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    height: 120px;
+    width: 90%;
+    margin-top: 30px;
+    border-style: dotted;
+    border-color: #9dc135;
+}
+.ts-main-head{
+    background-color: #9dc135;
+    height: 30px;
+    width: 40%;
+    text-align: center;
+    border-radius: 5px;
+    margin-left: -65%;
+    margin-top: -2%;
+}
+.ts-main-center{
+    margin-top: 20px;
+    font-size: 10px;
+    width: 80%;
+}
+</style>
