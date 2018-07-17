b40fa383842afb82d0ee83607cee305f2f46b148
sharon <403431641@qq.com>
2018-07-17 21:32:27 +0800

    update code

--- src/components/homepage/productInside.vue
+++ src/components/homepage/productInside.vue
@@ -1,116 +1,133 @@
 <template>
-<div class="proudceInside">
-    
-    <div class="product-body">
-               
-        <div v-for="(item,key) in items" :key="key">   
-           
-            <div class="picture"> 
-               <img class="product-picture" :src="item.imageArr[0]" alt="">
-               <div class="product-title">
-                   <span>{{item.title}}</span>
-               </div> 
-            </div>  
-            
+    <div class="td-container">
+        <div class="td-main">
+            <div class="td-main-img" v-for="(info1 , key) in info.cases" :key="key">
+                <silentbox-group>
+                    <silentbox-item v-for="(info2 , key2) in info1.imageArr" :key="key2" :src="info2">
+                        <img  v-if="key2===0" class="td-main-picture" :src="info1.imageArr[0]" alt="">
+                    </silentbox-item>
+                    <div class="td-main-title"><span>{{info1.title}}</span></div>
+                </silentbox-group>
+            </div>
         </div>
-          
-    </div> 
-    <div class="product-foot">
         <el-pagination
             layout="prev, pager, next"
-            :total="totalCount" :page-size="pageSize" :current-page="currentPage" @current-change="currentPageChanged"
+            :total="totalCount" :page-size="pageSize" :current-page="currentPage" @current-change="currentPageChanged" 
         >
         </el-pagination>
     </div>
-</div>
 </template>
-
 <script>
 export default {
     data() {
         return {
-            isLoading: true,
-            items:[
-
-            ],
+            info:{},
             currentPage:1,
-            totalCount:0,
-            pageSize:6
+            pageSize:6,
+            totalCount: 0
         }
- },
+    },
 created: function() {
+    console.log(this.$route.query.type);
     this.request();
   },
-  methods: {
-      currentPageChanged(val) {
-          this.currentPage = val;
-              this.request();
-        console.log(`当前页: ${val}`);
-      },
+  methods : {
       request() {
-              var that = this;
-              
-            this.$ajax.get('/cases', {
-                params: {
-                    type:this.$route.query.type,
-                    page:this.currentPage,
-                    pageSize:this.pageSize
-                }
+        var that = this;
+        this.$ajax.get('/getCaseByType', {
+            params:{
+                type:this.$route.query.type,
+                page:this.currentPage,
+                pageSize:this.pageSize
+            }
+        })
+        .then(function (response) {
+        console.log(response);
+        that.info = response.data.model.items;
+        that.totalCount = response.data.model.totalCount;
+        })
+        .catch(function (response) {
+        console.log(response);
+        });
+      },
+      currentPageChanged(val) {
+        this.currentPage = val;
+        this.request();
 
-            })
-            .then(function (response) {
-                console.log(response);
-                that.items = response.data.model.cases;
-                that.totalCount = response.data.model.totalCount
-                that.isLoading = false;
-            })
-            .catch(function (response) {
-                console.log(response);
-                that.isLoading = false;
-
-            });
       }
   }
 }
 </script>
 
 
-<style scoped>
-.productInside{
+<style lang="scss">
+.td-container{
+    // display: flex;
+    // flex-direction: column;
+    // align-items: center;
     margin: 0;
     padding: 0;
     display: flex;
     flex-direction: column;
     justify-content:center;
     align-items: center;
+    // border-bottom:2px solid #9dc135;
     width:100%;
+    background-color: aqua;
+
 }
 
-
-.product-body{
-    width:100%;   
+.td-main{
+    // margin-top: 80px;
+    // width: 900px;
+    // display: flex;
+    // flex-direction: row;
+    // flex-wrap: wrap;
+    width:320px;   
     display: flex;
     flex-wrap:wrap;
     justify-content:center;
-    align-items: center;   
+    // align-items: center; 
+    background-color: red; 
+    margin-bottom: 20px; 
    
-    margin-top:40px; 
-    
-}
+    // margin-top:40px; 
 
-.picture{
+}
+.td-main-img{
+    // width: 300px;
+    // display: flex;
+    // justify-content: center;
+    // align-items: flex-end;
     width:130px;
-    height: 80px;
+    height: 90px;
     margin:10px;
+    background-color: yellow;
+    
+
 }
 
-.product-picture{
+.td-main-picture{
+    // width: 270px;
+    // height: 200px; 
     width:130px;
-    height: 80px;
+    height: 90px;
     position:absolute;
     z-index: -1;
+   
+
 }
-.product-title{
+
+.td-main-title{
+    // position: relative;
+    // top:-60px;
+    // color: white;
+    // background-color: rgba(180, 218, 44, 0.5);
+    // width: 270px;
+    // height: 40px;
+    // display: flex;
+    // justify-content: center;
+    // align-items: center;
     width:130px;   
     background-color: rgba(174,203,87, 0.83);
     text-align: center;
@@ -121,11 +138,6 @@ created: function() {
     white-space:nowrap;
     overflow:hidden;
     text-overflow:ellipsis;
-}
-.product-foot{    
-    
-    margin-bottom: 40px;
 
 }
 </style>
-
